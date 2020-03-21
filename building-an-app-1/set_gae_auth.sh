a325cfc5dcb1a6fa852c26e6471a0975a97b5a95
Will Richardson <will.richardson@bblox.io>
2020-03-20 21:38:44 -0400

    replace with HOME env var

--- building-an-app-1/set_gae_auth.sh
+++ building-an-app-1/set_gae_auth.sh
@@ -3,4 +3,4 @@
 # key file from 
 # gcloud iam service-accounts keys create service_account.key.json --iam-account simple-webapp-269621@appspot.gserviceaccount.com
 
-export GOOGLE_APPLICATION_CREDENTIALS="/home/will/Development/my_gae_simple/building-an-app/service_account.key.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/Development/my_gae_simple/building-an-app/service_account.key.json"
